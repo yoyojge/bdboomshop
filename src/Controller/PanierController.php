e1a1ccb40cd81c8d8b542bdf3fc1b6e02c217727
= <=>
2023-01-05 09:13:18 +0100

    front - correction bug

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -100,8 +100,101 @@ class PanierController extends AbstractController
         return $this->redirectToRoute('app_shop_panierShow', [], Response::HTTP_SEE_OTHER);
     }
 
-    
 
+
+
+    // PAYPAL :: https://grafikart.fr/tutoriels/paypal-checkout-standard-962
+    public function ui(Cart $cart): string
+    {
+        $clientId = PAYPAL_ID;
+        $order = json_encode([
+            'purchase_units' => [
+                [
+                    'description' => 'Panier tutoriel grafikart',
+                    'items'       => array_map(function ($product) {
+                        return [
+                            'name'        => $product['name'],
+                            'quantity'    => 1,
+                            'unit_amount' => [
+                                'value'         => number_format($product['price'] / 100, 2, '.', ""), // Mes sommes sont en centimes d'euros
+                                'currency_code' => 'EUR',
+                            ]
+                        ];
+                    }, $cart->getProducts()),
+                    'amount'      => [
+                        'currency_code' => 'EUR',
+                        'value'         => number_format($cart->getTotal() / 100, 2, '.', ""),
+                        'breakdown'     => [
+                            'item_total' => [
+                                'currency_code' => 'EUR',
+                                'value'         => number_format($cart->getTotal() / 100, 2, '.', "")
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ]);
+        return <<<HTML
+        <script src="https://www.paypal.com/sdk/js?client-id={$clientId}&currency=EUR&intent=authorize"></script>
+        <div id="paypal-button-container"></div>
+        <script>
+          paypal.Buttons({
+            // Sets up the transaction when a payment button is clicked
+            createOrder: (data, actions) => {
+              return actions.order.create({$order});
+            },
+            // Finalize the transaction after payer approval
+            onApprove: async (data, actions) => {
+              const authorization = await actions.order.authorize()
+              const authorizationId = authorization.purchase_units[0].payments.authorizations[0].id
+              await fetch('/paypal.php', {
+                method: 'post',
+                headers: {
+                  'content-type': 'application/json'
+                },
+                body: JSON.stringify({authorizationId})
+              })
+              alert('Votre paiement a bien été enregistré')
+            }
+          }).render('#paypal-button-container');
+        </script>
+    HTML;
+    }
+
+
+    public function handle(ServerRequestInterface $request, Cart $cart): void
+    {
+        if ($this->sandbox) {
+            $environment = new \PayPalCheckoutSdk\Core\SandboxEnvironment($this->clientId, $this->clientSecret);
+        } else {
+            $environment = new \PayPalCheckoutSdk\Core\ProductionEnvironment($this->clientId, $this->clientSecret);
+        }
+        $client = new \PayPalCheckoutSdk\Core\PayPalHttpClient($environment);
+        $authorizationId = $request->getParsedBody()['authorizationId'];
+        $request = new \PayPalCheckoutSdk\Payments\AuthorizationsGetRequest($authorizationId);
+        $authorizationResponse = $client->execute($request);
+        if ($authorizationResponse->result->amount->value !== number_format($cart->getTotal() / 100, 2, '.', "")) {
+            throw new PaymentAmountMissmatchException($amount, $cart->getTotal());
+        }
+
+        // On peut récupérer l'Order créé par le bouton
+        $orderId = $authorizationResponse->result->supplementary_data->related_ids->order_id;
+        // $request = new OrdersGetRequest($orderId);
+        // $orderResponse = $client->execute($request);
+
+        // Vérifier si le stock est dispo
+
+        // Verrouiller le produit (retirer du stock pour éviter une commande en parallèle entre temps)
+
+        // Sauvegarder les informations de l'utilisateur
+
+        // On capture l'autorisation
+        $request = new AuthorizationsCaptureRequest($authorizationId);
+        $response = $client->execute($request);
+        if ($response->result->status !== 'COMPLETED') {
+            throw new \Exception();
+        }
+    }
     
 
 
